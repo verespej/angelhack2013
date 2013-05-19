c1d0193996b51b23ce13c985db6dd4d5357c44cf
Hakon Verespej <hakon@Hakons-MacBook-Air.local>
2013-05-19 12:46:57 -0700

    Add content to web page

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ var app = express();
 app.use(express.logger());
 app.use("/", express.static(__dirname + "/static"));
 
+var port = process.env.PORT || 32123;
+app.listen(port, function() {
+		console.log("Listening on " + port);
+});
+
